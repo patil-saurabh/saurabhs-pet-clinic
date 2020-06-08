1196f5e17ad2eb142840aba1200c3d7b8fc441a0
saupat22 <saupat22@gmail.com>
2020-06-08 20:25:07 +0530

    First commit, issue#1 is done.

---
+++ src/main/java/com/saurabh/saurabhspetclinic/SaurabhsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.saurabhspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaurabhsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaurabhsPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/saurabhspetclinic/SaurabhsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.saurabhspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaurabhsPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
